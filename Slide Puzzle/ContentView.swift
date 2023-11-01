0804c8f04266fc69df9189fd21a6c190e60b9a5c
NICHAPA <nichapa.por@dome.tu.ac.th>
2023-11-01 11:43:03 +0700

    Initial Commit

---
+++ Slide Puzzle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slide Puzzle
+//
+//  Created by Mind Nichapa on 1/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slide Puzzle/Slide_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slide_PuzzleApp.swift
+//  Slide Puzzle
+//
+//  Created by Mind Nichapa on 1/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Slide_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
